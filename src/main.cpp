213bb3780c47cd7c1998385f7678e0724ffa5cca
Slexandr <Sashu998@gmail.com>
2017-06-01 18:35:10 +0700

    Realise exit in main menu

--- src/main.cpp
+++ src/main.cpp
@@ -15,39 +15,43 @@ int main()
 	system("mode con cols=120 lines=30");
 	int sel = 0, ch = 0;
 	while (sel != 2) {
-		while(ch != 13){
-			if(sel == 0){
+		while (ch != 13) {
+			if (sel == 0) {
 				SetConsoleCursorPosition(hConsole, zero);
 				Intro();
 				Main_Menu_1();
 			}
-			if(sel == 1){
+			if (sel == 1) {
 				SetConsoleCursorPosition(hConsole, zero);
 				Intro();
 				Main_Menu_2();
 			}
-			if(sel == 2){
+			if (sel == 2) {
 				SetConsoleCursorPosition(hConsole, zero);
 				Intro();
 				Main_Menu_3();
 			}
 			ch = getch();
-			if(ch == 72){
+			if (ch == 72) {
 				sel--;
 			}
-			if(sel < 0){
+			if (sel < 0) {
 				sel = 2;
 			}
-			if(ch == 80){
+			if (ch == 80) {
 				sel++;
 			}
-			if(sel > 2){
+			if (sel > 2) {
 				sel = 0;
 			}
 		}
-		if(sel == 0){
+		if (sel == 0) {
 			Testing_main(Select_Theme());
 		}
+		if (sel == 2) {
+			return 0;
+		}
+		sel = 0;
+		ch = 0;
 	}
-	return 0;
 }
