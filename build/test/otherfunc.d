7f70043343235ac4bc6f2cbd5b3b7a7c50d2a5d2
Dmitriy8726 <Agch6411@gmail.com>
2017-06-02 17:38:32 +0700

    Test function proverka

--- build/test/otherfunc.d
+++
@@ -1,3 +0,0 @@
-build/test/otherfunc.o: test/otherfunc2.c test/otherfunc2.h
-
-test/otherfunc2.h:
--- test/otherfunc_test.c
+++ test/otherfunc_test.c
@@ -1,9 +1,36 @@
 #include <ctest.h>
 #include <../test/otherfunc2.h>
 
-CTEST(Test_one, Language)
+CTEST(Test_1, Language_h)
 {
     int result = Proverka('h');
+	
+    int expected = 1;
+	
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Test_2, Language_H)
+{
+    int result = Proverka('H');
+	
+    int expected = 1;
+	
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Test_3, Language_p)
+{
+    int result = Proverka('р');
+	
+    int expected = 1;
+	
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Test_4, Language_P)
+{
+    int result = Proverka('Р');
 	
     int expected = 1;
 	
